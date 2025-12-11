96ce9d84f4c907fffebde277f07ae456de37c6b5
TecPty <chatsomos@gmail.com>
2025-12-11 00:29:34 -0500

    creacion de New West

--- components/property-filters.tsx
+++ components/property-filters.tsx
@@ -20,7 +20,7 @@ const locations = [
   "Coronado",
 ]
 
-const propertyTypes = ["Apartamento", "Casa", "Villa", "Lote", "Local", "Oficina", "Terreno"]
+const propertyTypes = ["Apartamento", "Casa", "Villa", "Local", "Oficina"]
 
 export default function PropertyFiltersComponent({ filters, onFiltersChange, onClear }: PropertyFiltersProps) {
   const handleTypeToggle = (type: string) => {
